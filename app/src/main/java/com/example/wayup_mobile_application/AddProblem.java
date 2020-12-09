b8b55f725941104fcbff86f42a92134bff7e1b5f
AnzePeharc <anze.peharc@gmail.com>
2020-12-09 19:30:49 +0100

    Implemented the basic structure for adding new problems to the database

--- app/src/main/java/com/example/wayup_mobile_application/AddProblem.java
+++ app/src/main/java/com/example/wayup_mobile_application/AddProblem.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,13 +20,16 @@ import java.util.function.ToDoubleBiFunction;
 
 public class AddProblem extends Activity implements AdapterView.OnItemSelectedListener{
 
-    String[] grades = { "4a", "4a+", "4b", "4b+", "4c", "4c+", "5a", "5a+", "5b", "5b+", "5c", "5c+", "6a", "6a+", "6b", "6b+", "6c", "6c+",
+    String[] grades = { "Select grade of the problem","4a", "4a+", "4b", "4b+", "4c", "4c+", "5a", "5a+", "5b", "5b+", "5c", "5c+", "6a", "6a+", "6b", "6b+", "6c", "6c+",
             "7a", "7a+", "7b", "7b+", "7c", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+"};
     Button back;
     Button discard;
     Button save;
     Spinner spinner;
     AlertDialog dialog;
+    EditText name;
+    EditText setter;
+    EditText comment;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +38,17 @@ public class AddProblem extends Activity implements AdapterView.OnItemSelectedLi
         back = (Button) findViewById(R.id.back_button);
         discard = (Button) findViewById(R.id.discard_button);
         save = (Button) findViewById(R.id.save_button); //TODO implement onclicklistener for adding problems to the database
+        name = (EditText) findViewById(R.id.name_input);
+        setter = (EditText) findViewById(R.id.setter_input);
+        comment = (EditText) findViewById(R.id.comment_input);
         spinner = (Spinner) findViewById(R.id.grade_spinner);
-
+        // set the default value of the spinner to the first element from array - grades
+        spinner.setSelection(0);
         // set onclicklistener for spinner
         spinner.setOnItemSelectedListener(this);
         //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter spinner_adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,grades);
-        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter spinner_adapter = new ArrayAdapter(this,R.layout.spinner_item,grades);
+        // spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         spinner.setAdapter(spinner_adapter);
 
@@ -70,6 +78,22 @@ public class AddProblem extends Activity implements AdapterView.OnItemSelectedLi
                     }
                 });
                 dialog = builder.show();
+
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"You need to add the name of the problem!" , Toast.LENGTH_LONG).show();
+                }
+                if(setter.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"You need to add the name of the problem!" , Toast.LENGTH_LONG).show();
+                }
+                if(spinner.getSelectedItem().toString().equals("Select grade of the problem")){
+                    Toast.makeText(getApplicationContext(),"You must choose the grade of the problem from dropdown!" , Toast.LENGTH_LONG).show();
+                }
 
             }
         });
