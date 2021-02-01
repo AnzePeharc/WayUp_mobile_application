01151c9a29cca5dc42b9abe074dbf2a4df842fc8
AnzePeharc <anze.peharc@gmail.com>
2021-02-01 16:31:35 +0100

    Added animation to the splash screen

--- app/src/main/java/com/example/wayup_mobile_application/SplashActivity.java
+++ app/src/main/java/com/example/wayup_mobile_application/SplashActivity.java
@@ -3,16 +3,32 @@ package com.example.wayup_mobile_application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class SplashActivity extends AppCompatActivity {
     Handler handler;
+
+    //Animation variables
+    Animation start_animation;
+
+    ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+        // Initialize the animation variable
+        start_animation = AnimationUtils.loadAnimation(this, R.anim.splash_screen_animation);
+        // Initialize the ImageView variable
+        logo = findViewById(R.id.logo_id);
+        // set the animation
+        logo.setAnimation(start_animation);
 
         handler= new Handler();
         handler.postDelayed(new Runnable() {
